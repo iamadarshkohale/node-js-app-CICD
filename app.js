4fbee4f49d94b3f9226cfaabdcad90011aa19e81
iamadarshkohale <adarshkohale@gmail.com>
2025-08-13 20:33:06 +0530

    modified file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('HIII from Jenkins & PM2 deployed Node.js App!using jenkins');
+  res.send('HIII from Jenkins & PM2 deployed Node.js App!using jenkins and jenkinsfile');
 });
 
 app.listen(port, () => {
